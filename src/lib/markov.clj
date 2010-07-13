4e4ff48200fb1ae3f735b712c0c1c8c9bf77496e
Ryan Spangler <rspangler@ryan-spangler.lan>
2010-07-13 03:15:34 -0700

    testing markov

--- src/lib/markov.clj
+++ src/lib/markov.clj
@@ -4,40 +4,66 @@
 (defstruct markov-wheel :slices :total)
 (defstruct markov-slice :token :weight)
 (defstruct markov-node :token :incoming :outgoing :beginning :ending)
-(defstruct markov-chain :nodes :beginnings :endings)
+(defstruct markov-chain :nodes :beginning :ending)
 
 (defn empty-wheel [] (struct markov-wheel {} 0))
-(defn empty-chain [] (struct markov-chain {} [] []))
+(defn empty-chain [] (struct markov-chain {} (empty-wheel) (empty-wheel)))
 
-(defn inc-slice [slice]
+(defn inc-slice 
+  [slice]
   (assoc slice :weight (+ (slice :weight) 1)))
 
-(defn wheel-slice [wheel token]
-  (or ((wheel :slices) token) (struct markov-slice token 0)))
+(defn wheel-slice 
+  [wheel token]
+  (or (-> wheel :slices token) (struct markov-slice token 0)))
 
-(defn wheel-weight [wheel token]
-  (((wheel :slices) token) :weight))
+(defn wheel-weight 
+  [wheel token]
+  ((-> wheel :slices token) :weight))
 
-(defn collect-token [wheel token]
+(defn wheel-token 
+  [wheel token]
   (let [slice (inc-slice (wheel-slice wheel token))]
+    (pr slice)
     (struct markov-wheel (assoc (wheel :slices) token slice) (+ (wheel :total) 1))))
 
-(defn spin [wheel]
-  (let [fate (* (rand) (wheel :total))]
-    (loop [tokens (seq (keys (wheel :slices)))
-           step 0]
-      (let [next-step (+ step (wheel-weight wheel (first tokens)))]
-        (if (> fate next-step)
-          (recur (next tokens) next-step)
-          (first tokens))))))
+(defn spin 
+  [wheel]
+  (if (empty? (wheel :slices))
+    nil
+    (let [fate (* (rand) (wheel :total))]
+      (loop [tokens (seq (keys (wheel :slices)))
+             step 0]
+        (let [next-step (+ step (wheel-weight wheel (first tokens)))]
+          (if (> fate next-step)
+            (recur (next tokens) next-step)
+            (first tokens)))))))
 
-(defn chain-node [chain token]
-  (or ((chain :nodes) token) (struct markov-node token (empty-wheel) (empty-wheel) false false)))
+(defn node-for
+  [chain token]
+  (or (-> chain :nodes token) (struct markov-node token (empty-wheel) (empty-wheel) false false)))
 
-(defn add-node [node orientation token]
-  (assoc node orientation (collect-token (node orientation) token)))
+(defn add-token 
+  [node orientation token]
+  (assoc node orientation (wheel-token (node orientation) token)))
 
-(defn add-link [chain from-token to-token]
-  (let [[from-node to-node] [(add-node (chain-node chain from-token) :outgoing to-token) (add-node (chain-node chain to-token) :incoming from-token)]]
+(defn add-link 
+  [chain from-token to-token]
+  (let [[from-node to-node] [(add-token (node-for chain from-token) :outgoing to-token) (add-token (node-for chain to-token) :incoming from-token)]]
     (struct markov-chain (assoc (assoc (chain :nodes) from-token from-node) to-token to-node))))
+
+(defn add-terminal
+  [chain terminal token]
+  (assoc chain terminal (wheel-token (chain terminal) token)))
+
+(defn add-token-stream
+  [chain tokens]
+  (let [head (first tokens)]
+    (if head
+      (loop [chain (add-terminal chain :beginning head)
+             tokens (rest tokens)
+             previous head]
+        (if (empty? tokens)
+          (add-terminal chain :ending previous)
+          (recur (add-link chain previous (first tokens)) (rest tokens) (first tokens)))))))
 
--- test/omnibus/markov_test.clj
+++ test/omnibus/markov_test.clj
@@ -1,10 +1,18 @@
 (ns omnibus.markov-test
+  (:use [clojure.contrib.seq-utils :only (includes?)])
   (:use [lib.markov] :reload-all)
   (:use [clojure.test]))
 
-(def wheel (collect-token (collect-token (collect-token (collect-token (empty-wheel) "nonon") "beleb") "nonon") "ppppp"))
+(deftest wheel-test
+  (let [wheel (reduce #'wheel-token (empty-wheel) [:nonon :beleb :nonon :ppppp])]
+    (is (= 4 (wheel :total)))
+    (is (= 2 (-> (wheel-slice wheel :nonon) :weight)))
+    (is (= 1 (wheel-weight wheel :beleb)))
+    (is (includes? [:nonon :beleb :ppppp] (spin wheel)))
+    (is (not (spin (-> (node-for (empty-chain) :nonon) :outgoing))))))
 
-(deftest markov-test
-  (is (= 4 (wheel :total)))
-  (is (= 2 ((wheel-slice wheel "nonon") :weight)))
-)
+(deftest chain-test
+  (let [chain (reduce #(add-link %1 (first %2) (last %2)) (empty-chain) [[:nononon :pelpelp] [:yelyely :bonobon]])]
+    (is (= 4 (count (chain :nodes))))
+    (is (= 1 (-> (add-terminal chain :beginning :graararaa) :beginning :total)))))
+
