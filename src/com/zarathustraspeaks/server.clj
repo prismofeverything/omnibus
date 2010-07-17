d5c0f81140caf8fa21922078fa6400b4693422f3
Ryan Spangler <rspangler@ryan-spangler.lan>
2010-07-17 15:50:56 -0700

    got zarathustra generating statements

--- src/com/zarathustraspeaks/server.clj
+++ src/com/zarathustraspeaks/server.clj
@@ -1,11 +1,16 @@
 (ns com.zarathustraspeaks.server
   (:use compojure.core
         ring.util.servlet
-        ring.adapter.jetty)
+        ring.adapter.jetty
+        lib.markov)
   (:require [compojure.route :as route]))
 
+(def zarathustra-chain (read-source "text/zabbrev.txt"))
+
 (defroutes zarathustraspeaks
-  (GET "/" [] "OH YES I SEE")
-  (route/not-found "NONONONNONONONON"))
+  (GET "/" [] (generate-statement zarathustra-chain))
+  (route/not-found (generate-statement zarathustra-chain)))
 
 (def zarathustraspeaks-server (run-jetty zarathustraspeaks {:port 8188 :join? false}))
+
+
--- src/lib/markov.clj
+++ src/lib/markov.clj
@@ -1,5 +1,6 @@
 (ns lib.markov
-  (:use [clojure.contrib.duck-streams :only (read-lines)]))
+  (:use clojure.contrib.str-utils
+        clojure.contrib.duck-streams))
 
 (defstruct markov-wheel :slices :total)
 (defstruct markov-slice :token :weight)
@@ -15,13 +16,13 @@
 
 (defn wheel-slice 
   [wheel token]
-  (or (-> wheel :slices token) (struct markov-slice token 0)))
+  (or ((wheel :slices) token) (struct markov-slice token 0)))
 
 (defn wheel-weight 
   [wheel token]
-  ((-> wheel :slices token) :weight))
+  (((wheel :slices) token) :weight))
 
-(defn wheel-token 
+(defn observe-token 
   [wheel token]
   (let [slice (inc-slice (wheel-slice wheel token))]
     (struct markov-wheel (assoc (wheel :slices) token slice) (+ (wheel :total) 1))))
@@ -40,20 +41,26 @@
 
 (defn node-for
   [chain token]
-  (or (-> chain :nodes token) (struct markov-node token (empty-wheel) (empty-wheel) false false)))
+  (or ((chain :nodes) token) (struct markov-node token (empty-wheel) (empty-wheel) (empty-wheel) (empty-wheel))))
 
-(defn add-token 
+(defn continuing-node
+  [chain token terminal value]
+  (let [node (or ((chain :nodes) token) (struct markov-node token (empty-wheel) (empty-wheel) (empty-wheel) (empty-wheel)))]
+    (assoc node terminal (observe-token (node terminal) value))))
+
+(defn add-orientation
   [node orientation token]
-  (assoc node orientation (wheel-token (node orientation) token)))
+  (assoc node orientation (observe-token (node orientation) token)))
 
-(defn add-link 
+(defn add-link
   [chain from-token to-token]
-  (let [[from-node to-node] [(add-token (node-for chain from-token) :outgoing to-token) (add-token (node-for chain to-token) :incoming from-token)]]
-    (assoc chain :nodes (assoc (assoc (chain :nodes) from-token from-node) to-token to-node))))
+  (let [from (assoc-in chain [:nodes from-token] (add-orientation (continuing-node chain from-token :ending :false) :outgoing to-token))]
+    (assoc-in from [:nodes to-token] (add-orientation (continuing-node from to-token :beginning :false) :incoming from-token))))
 
 (defn add-terminal
   [chain terminal token]
-  (assoc chain terminal (wheel-token (chain terminal) token)))
+  (let [terminated (assoc chain terminal (observe-token (chain terminal) token))]
+    (assoc-in terminated [:nodes token] (continuing-node terminated token terminal :true))))
 
 (defn add-token-stream
   [chain tokens]
@@ -65,4 +72,23 @@
         (if (empty? tokens)
           (add-terminal chain :ending previous)
           (recur (add-link chain previous (first tokens)) (rest tokens) (first tokens)))))))
+
+(defn follow-strand
+  [chain]
+  (loop [token (spin (chain :beginning))
+         path (list token)
+         node ((chain :nodes) token)]
+    (if (= (spin (node :ending)) :true)
+      (reverse path)
+      (let [follow (spin (node :outgoing))]
+        (recur follow (cons follow path) ((chain :nodes) follow))))))
+
+(defn read-source
+  [path]
+  (let [source (re-split #"  " (re-gsub #": " ":" (str-join " " (read-lines path))))]
+    (reduce add-token-stream (empty-chain) (map #(re-split #" " %) source))))
+
+(defn generate-statement
+  [chain]
+  (str-join " " (follow-strand chain)))
 
