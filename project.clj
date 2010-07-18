d102606fe56b1ce3e9224ceafbc7ec02f1c3677c
Ryan Spangler <rspangler@ryan-spangler.lan>
2010-07-18 03:37:16 -0700

    zarathustra html generation

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [compojure "0.4.0"]
-                 [ring/ring-jetty-adapter "0.2.3"]]
+                 [ring/ring-jetty-adapter "0.2.3"]
+                 [hiccup "0.2.6"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
--- src/com/zarathustraspeaks/server.clj
+++ src/com/zarathustraspeaks/server.clj
@@ -2,14 +2,29 @@
   (:use compojure.core
         ring.util.servlet
         ring.adapter.jetty
-        lib.markov)
+        lib.markov
+        hiccup.core)
   (:require [compojure.route :as route]))
 
 (def zarathustra-chain (read-source "text/zabbrev.txt"))
 
+(defn zarathustra-home
+  [statement]
+  (html
+   [:html
+    [:head
+     [:title "Zarathustra SPEAKS"]
+     [:link {:rel "stylesheet" :type "text/css" :href "/css/zarathustra.css"}]]
+    [:body
+     [:div#speaks
+      [:p
+       [:a#title {:href "/"} "ZARATHUSTRA SAYS:"]]]
+     [:div#statement
+      [:p statement]]]]))
+
 (defroutes zarathustraspeaks
-  (GET "/" [] (generate-statement zarathustra-chain))
-  (route/not-found (generate-statement zarathustra-chain)))
+  (GET "/" [] (zarathustra-home (generate-statement zarathustra-chain)))
+  (route/not-found (zarathustra-home (generate-statement zarathustra-chain))))
 
 (def zarathustraspeaks-server (run-jetty zarathustraspeaks {:port 8188 :join? false}))
 
