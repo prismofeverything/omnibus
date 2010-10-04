0e77b5b43c9bdf65be9d8ba7e647ca64b4883ddd
Ryan Spangler <rspangler@ryan-spangler.lan>
2010-10-04 12:10:58 -0700

    work on ik

--- sites/omniomnib.us/public/js/ik.js
+++ sites/omniomnib.us/public/js/ik.js
@@ -1,13 +1,15 @@
 var ik = function() {
-    var maxlevel = 5;
+    var maxlevel = 20;
     var zoom = 500;
+    var move = [400, 200];
+    var colorFactor = 127;
 
     var above = function(level) {
         return (maxlevel - level) > 0 ? (maxlevel - level) : 0;
     };
 
     var dot = function(a, b) {
-            var uu = 0;
+        var uu = 0;
         for (var oo = 0; oo < Math.min(a.length, b.length); oo++) {
             uu += a[oo] * b[oo];
         }
@@ -28,7 +30,7 @@ var ik = function() {
     var colorhex = function(color) {
         var hex = '';
         for (var o = 0; o < 3; o++) {
-            var trans = Math.min(Math.floor(255 * color[o]), 255).toString(16);
+            var trans = Math.min(Math.floor(colorFactor * Math.abs(color[o])), 255).toString(16);
             while (trans.length < 2) {
                 trans = '0' + trans;
             }
@@ -45,16 +47,28 @@ var ik = function() {
         };
     };
 
-    var transforms = {
-        aa: {
-            pos: [0.3, 0.8, 0.7, 0.1],
-            color: [0.1, 0.3, 0.5, 0.4, 0.3, 0.4, 0.2, 0.2, 0.3],
-            cycle: function(pod) {
-                if (pod.children.length === 0) {
-                    pod.add(pod.tag);
-                }
-            }
+    var randomTransform = function(dim) {
+        var trans = [];
+        for (var tt = 0; tt < dim; tt++) {
+            trans[tt] = (Math.random() * 2) - 1;
         }
+        return trans;
+    };
+
+    var cyclic = function(pod) {
+        if (pod.children.length === 0) {
+            var chosen = Math.floor(Math.random() * 3);
+            pod.add(chosen);
+        }
+    };
+
+    var transforms = [];
+    for (var ll = 0; ll < 3; ll++) {
+        transforms[ll] = transform(randomTransform(4), randomTransform(9), cyclic);
+    }
+
+    var shift = function(pos) {
+        return [(pos[0] * zoom) + move[0], (pos[1] * zoom) + move[1]];
     };
 
     var node = function(tag, level, origin, color) {
@@ -70,9 +84,11 @@ var ik = function() {
             },
 
             draw: function(jji) {
+                var shifted = shift(this.pos);
+
                 jji.fillStyle = '#' + colorhex(this.color);
                 jji.beginPath();
-                jji.arc(this.pos[0] * zoom, this.pos[1] * zoom, above(this.level) * 5, 0, Math.PI * 2);
+                jji.arc(shifted[0], shifted[1], 100/(this.level + 1), 0, Math.PI * 2);
 
                 if (above(this.level) > 1) {
                     for (var c = 0; c < this.children.length; c++) {
@@ -84,7 +100,7 @@ var ik = function() {
             },
 
             cycle: function(cyc) {
-                for (var c = 0; c < this.children; c++) {
+                for (var c = 0; c < this.children.length; c++) {
                     var child = this.children[c];
                     child.cycle(transforms[child.tag].cycle);
                 }
@@ -109,12 +125,14 @@ var ik = function() {
 
         var jji = mn.getContext('2d');
 
-        var roots = [node('aa', 0, [0.5, 0.5], [0.1, 0.1, 0.5]),
-                     node('aa', 0, [0.7, 0.3], [0.8, 0.8, 0.8])];
+        var roots = [];
+        for (var oo = 0; oo < 3; oo++) {
+            roots[oo] = node(oo, 0, randomTransform(2), randomTransform(3));
+        }
 
         cycle(jji, roots);
 
-        setInterval(function() {cycle(jji, roots);}, 1000);
+        setInterval(function() {cycle(jji, roots);}, 80);
     };
 
     return {
